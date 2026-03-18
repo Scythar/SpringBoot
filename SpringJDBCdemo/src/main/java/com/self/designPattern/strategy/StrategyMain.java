52d140f4fa2c2c05473f113b82ba51607ad5943f
Scyther <niteshsingh5375@gmail.com>
2026-03-18 14:08:15 +0530

    setup new connection in new laptop

--- SpringJDBCdemo/src/main/java/com/self/designPattern/strategy/StrategyMain.java
+++ SpringJDBCdemo/src/main/java/com/self/designPattern/strategy/StrategyMain.java
@@ -4,9 +4,10 @@ import com.self.SpringJDBCdemo.SpringJdbcDemoApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import strategy.service.PricingService;
-import strategy.strategy.DiscountStrategy;
-import strategy.strategy.FestivalDiscount;
+import com.self.designPattern.strategy.service.PricingService;
+import com.self.designPattern.strategy.strategy.DiscountStrategy;
+import com.self.designPattern.strategy.strategy.FestivalDiscount;
+
 @SpringBootApplication
 public class StrategyMain {
     public static void main(String[] args) {
