32328117600f560fd5abc133a5d56ba7714db4dc
Scyther <niteshsingh5375@gmail.com>
2026-03-25 14:22:56 +0530

    more clear on design pattern

--- SpringJDBCdemo/src/main/java/com/self/designPattern/factory/FactoryMain.java
+++ SpringJDBCdemo/src/main/java/com/self/designPattern/factory/FactoryMain.java
@@ -1,11 +1,15 @@
 package com.self.designPattern.factory;
 
 import com.self.designPattern.factory.factory.NotificationFactory;
+import com.self.designPattern.factory.notification.Notification;
 
 public class FactoryMain {
+
+    //    public static NotificationFactory notificationFactory;
     public static void main(String[] args) {
-        NotificationFactory notificationFactory = new NotificationFactory();
-        notificationFactory.createNotification("email");
-        notificationFactory.createNotification("sms");
+        Notification notification =
+                NotificationFactory.createNotification("email");
+
+        notification.send();
     }
 }
--- SpringJDBCdemo/src/main/java/com/self/designPattern/factory/factory/NotificationFactory.java
+++ SpringJDBCdemo/src/main/java/com/self/designPattern/factory/factory/NotificationFactory.java
@@ -1,9 +1,18 @@
 package com.self.designPattern.factory.factory;
 
 import com.self.designPattern.factory.notification.EmailNotification;
+import com.self.designPattern.factory.notification.Notification;
+import com.self.designPattern.factory.notification.SmsNotification;
 
 public class NotificationFactory {
-   public void createNotification(String type){
+    /* remember to declare method createNotification as static type
+
+    Static methods are perfect for utility classes that just perform operations.
+
+    Make a method static when it does NOT depend on object state (instance variables)
+    — regardless of whether other instance variables exist in the class or not.
+    */
+   public static Notification createNotification(String type){
        if(type.equalsIgnoreCase("email")){
            return new EmailNotification();
        }
